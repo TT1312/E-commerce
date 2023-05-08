439733815884227de961a66d7e382e9d702573c6
TT1312 <132920286+TT1312@users.noreply.github.com>
2023-05-08 21:09:52 +0530

    Delete SmartContactManager directory

--- SmartContactManager/src/main/java/com/smartcontactmanager/dao/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartcontactmanager.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.smartcontactmanager.entities.Contacts;
-
-public interface ContactRepository extends JpaRepository<Contacts,Integer>{
-	
-}
